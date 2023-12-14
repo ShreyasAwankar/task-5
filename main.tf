7f87ded1a480bcf81217b35bd8ffe942dbba559c
ShreyasAwankar <shreyasawankar01@gmail.com>
2023-12-14 14:16:30 +0530

    Initial commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "practice_bucket_task5" {
-  project       = "terraform-cloud-functions-ems"
+  project       = "practice_bucket_task5"
   name          = "no-public-access-bucket"
   location      = "US"
   force_destroy = true
